1c01919b89abcfa733ca28b108e1a29922e1ed84
simzub <78310955+simzub@users.noreply.github.com>
2023-09-28 13:35:53 +0300

    add dynamic changes on graph while hovering

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -16,4 +16,59 @@ export class IntroductionComponent {
     { name: 'Fast-Company', width: 124 },
     { name: 'HuffPost', width: 142 },
   ];
+
+  data = [
+    { price: 997.56, percentage: null },
+    { price: 944.34, percentage: -5.34 },
+    { price: 972.25, percentage: 2.96 },
+    { price: 832.4, percentage: -14.38 },
+    { price: 888.76, percentage: 6.77 },
+    { price: 834.8, percentage: -6.07 },
+    { price: 805.56, percentage: -3.5 },
+    { price: 767.38, percentage: -4.74 },
+    { price: 861.21, percentage: 12.23 },
+    { price: 669.6, percentage: -22.25 },
+    { price: 694.39, percentage: 3.7 },
+    { price: 721.32, percentage: 3.88 },
+    { price: 694.03, percentage: -3.78 },
+    { price: 610.1, percentage: -12.09 },
+    { price: 502.2, percentage: -17.69 },
+    { price: 549.56, percentage: 9.43 },
+    { price: 611.03, percentage: 11.19 },
+    { price: 583.4, percentage: -4.52 },
+    { price: 610.14, percentage: 4.58 },
+    { price: 660.6, percentage: 8.27 },
+    { price: 752.11, percentage: 13.85 },
+    { price: 721.19, percentage: -4.11 },
+    { price: 638.89, percentage: -11.41 },
+    { price: 661.7, percentage: 3.57 },
+    { price: 694.51, percentage: 4.96 },
+    { price: 580.3, percentage: -16.44 },
+    { price: 638.0, percentage: 9.94 },
+    { price: 613.3, percentage: -3.87 },
+    { price: 651.64, percentage: 6.25 },
+    { price: 560.51, percentage: -13.98 },
+    { price: 611.45, percentage: 9.09 },
+    { price: 670.68, percentage: 9.69 },
+    { price: 752.56, percentage: 12.21 },
+  ];
+
+  isHovering: boolean = false;
+  currentObjectId: number = this.data.length - 1;
+  percentageStyle: { [key: string]: string } = { color: 'rgb(168 85 247)' };
+
+  handleHoverChange(eventData: { isHovering: boolean; objectId: number }) {
+    this.isHovering = eventData.isHovering;
+    this.currentObjectId = eventData.objectId;
+
+    // Dynamic color accent
+    const selectedData = this.data[eventData.objectId];
+    if (selectedData && selectedData.percentage !== null) {
+      if (selectedData.percentage < 0) {
+        this.percentageStyle = { color: 'rgb(107, 114, 128)' };
+      } else {
+        this.percentageStyle = { color: 'rgb(168 85 247)' };
+      }
+    }
+  }
 }
--- src/assets/svg/svg-graph/svg-plus/svg-graph.component.ts
+++ src/assets/svg/svg-graph/svg-plus/svg-graph.component.ts
@@ -1,40 +1,122 @@
-import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  HostListener,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-svg-graph',
   templateUrl: './svg-graph.component.html',
 })
 export class SvgGraphComponent {
-  @ViewChild('svgElement') svgElement: ElementRef | undefined; // Initialize as undefined
+  @ViewChild('svgElement') svgElement: ElementRef | undefined;
+  @Output() hoverChange = new EventEmitter<{
+    isHovering: boolean;
+    objectId: number;
+  }>();
 
-  // Track mouse coordinates
-  mouseX: number = 0;
-  xValue: number[] = [
-    16, 23.9375, 31.875, 39.8125, 47.75, 55.6875, 63.625, 71.5625, 79.5,
-    87.4375, 95.375, 103.3125, 111.25, 119.1875, 127.125, 135.0625, 143,
-    150.9375, 158.875, 166.8125, 174.75, 182.6875, 190.625, 198.5625, 206.5,
-    214.4375, 222.375, 230.3125, 238.25, 246.1875, 254.125, 262.0625, 270,
+  isHovering: boolean = false;
+  data = [
+    { id: 0, x: 16, y: 32 },
+    { id: 1, x: 23.9375, y: 47.470930232558125 },
+    { id: 2, x: 31.875, y: 39.35755813953487 },
+    { id: 3, x: 39.8125, y: 80.01162790697674 },
+    { id: 4, x: 47.75, y: 63.62790697674417 },
+    { id: 5, x: 55.6875, y: 79.31395348837209 },
+    { id: 6, x: 63.625, y: 87.81395348837209 },
+    { id: 7, x: 71.5625, y: 98.91279069767441 },
+    { id: 8, x: 79.5, y: 71.63662790697671 },
+    { id: 9, x: 87.4375, y: 127.33720930232556 },
+    { id: 10, x: 95.375, y: 120.13081395348837 },
+    { id: 11, x: 103.3125, y: 112.30232558139531 },
+    { id: 12, x: 111.25, y: 120.23546511627907 },
+    { id: 13, x: 119.1875, y: 144.63372093023253 },
+    { id: 14, x: 127.125, y: 176 },
+    { id: 15, x: 135.0625, y: 162.2325581395349 },
+    { id: 16, x: 143, y: 144.36337209302326 },
+    { id: 17, x: 150.9375, y: 152.3953488372093 },
+    { id: 18, x: 158.875, y: 144.62209302325581 },
+    { id: 19, x: 166.8125, y: 129.953488372093 },
+    { id: 20, x: 174.75, y: 103.3517441860465 },
+    { id: 21, x: 182.6875, y: 112.34011627906975 },
+    { id: 22, x: 190.625, y: 136.2645348837209 },
+    { id: 23, x: 198.5625, y: 129.63372093023256 },
+    { id: 24, x: 206.5, y: 120.09593023255812 },
+    { id: 25, x: 214.4375, y: 153.296511627907 },
+    { id: 26, x: 222.375, y: 136.52325581395348 },
+    { id: 27, x: 230.3125, y: 143.70348837209303 },
+    { id: 28, x: 238.25, y: 132.5581395348837 },
+    { id: 29, x: 246.1875, y: 159.04941860465118 },
+    { id: 30, x: 254.125, y: 144.24127906976742 },
+    { id: 31, x: 262.0625, y: 127.0232558139535 },
+    { id: 32, x: 270, y: 103.22093023255815 },
   ];
-
-  constructor() {
-    // Initialize the property in the constructor
-    this.svgElement = undefined;
-  }
+  currentObject: { id: number; x: number; y: number } | null = null;
 
   updateMouseCoordinates(event: MouseEvent) {
     if (this.svgElement) {
-      // Get the mouse X coordinate relative to the SVG element
       const svgRect = this.svgElement.nativeElement.getBoundingClientRect();
-      this.mouseX = event.clientX - svgRect.left;
+      const mouseX = event.clientX - svgRect.left;
+
+      // Binary search function to find the closest object
+      const findClosest = (
+        arr: { id: number; x: number; y: number }[],
+        target: number
+      ) => {
+        let left = 0;
+        let right = arr.length - 1;
+        let closestIndex = -1;
+        let closestDistance = Number.MAX_VALUE;
+
+        while (left <= right) {
+          const mid = Math.floor((left + right) / 2);
+          const distance = Math.abs(arr[mid].x - target);
+
+          if (distance < closestDistance) {
+            closestIndex = mid;
+            closestDistance = distance;
+          }
+
+          if (arr[mid].x < target) {
+            left = mid + 1;
+          } else {
+            right = mid - 1;
+          }
+        }
+
+        return closestIndex;
+      };
+
+      // Find the closest object using binary search
+      const closestIndex = findClosest(this.data, mouseX);
+      const closestObject = this.data[closestIndex];
+      this.currentObject = closestObject;
+      this.hoverChange.emit({
+        isHovering: this.isHovering,
+        objectId: closestObject.id,
+      });
     }
   }
 
-  // Listen for mousemove events on the SVG element
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     this.updateMouseCoordinates(event);
+  }
 
-    // You can do something with the mouse coordinates here
-    console.log(`Mouse X: ${this.mouseX}`);
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.isHovering = true;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.isHovering = false;
+    this.hoverChange.emit({
+      isHovering: false,
+      objectId: 32,
+    });
   }
 }
