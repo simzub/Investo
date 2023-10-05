99621bdaf6b4d2f2e0ed783d2b2633d92d5dd891
simzub <78310955+simzub@users.noreply.github.com>
2023-10-05 09:37:15 +0300

    optimized phone content stocks component

--- src/app/primary-features/phone-content/phone-content-stocks/phone-content-stocks.component.ts
+++ src/app/primary-features/phone-content/phone-content-stocks/phone-content-stocks.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-phone-content-stocks',
   templateUrl: './phone-content-stocks.component.html',
-  styleUrls: ['./phone-content-stocks.component.scss']
+  styleUrls: ['./phone-content-stocks.component.scss'],
 })
 export class PhoneContentStocksComponent {
-
+  stockItems = [
+    { title: 'Laravel', price: '4,098.01', change: 4.98 },
+    { title: 'Tuple', price: '5,451.10', change: -3.38 },
+    { title: 'Transistor', price: '4,098.41', change: 6.25 },
+    { title: 'Diageo', price: '250.65', change: 1.25 },
+    { title: 'StaticKit', price: '250.65', change: -3.38 },
+    { title: 'Statamic', price: '5,040.85', change: -3.11 },
+    { title: 'Mirage', price: '140.44', change: 9.09 },
+    { title: 'Reversable', price: '550.60', change: -1.25 },
+  ];
 }
