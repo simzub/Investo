cb45bce98485b65af9d9a61ec1d359995798f3d4
simzub <78310955+simzub@users.noreply.github.com>
2023-10-07 14:01:40 +0300

    finished scrolling logic on primary features mobile component

--- src/app/primary-features/features-mobile/features-mobile.component.ts
+++ src/app/primary-features/features-mobile/features-mobile.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-features-mobile',
   templateUrl: './features-mobile.component.html',
-  styleUrls: ['./features-mobile.component.scss']
+  styleUrls: ['./features-mobile.component.scss'],
 })
-export class FeaturesMobileComponent {
+export class FeaturesMobileComponent implements OnInit {
+  activeSlideIndex = 0;
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {}
+
+  setActiveSlide(index: number) {
+    this.activeSlideIndex = index;
+    const layoutElement = this.el.nativeElement.querySelector('.layout');
+    const scrollWidth = layoutElement.scrollWidth;
+    const clientWidth = layoutElement.clientWidth;
+
+    switch (this.activeSlideIndex) {
+      case 0:
+        this.setScrollBarPosition(layoutElement, 0);
+        break;
+      case 1:
+        this.setScrollBarPosition(
+          layoutElement,
+          (scrollWidth - clientWidth) / 2
+        );
+        break;
+      case 2:
+        this.setScrollBarPosition(layoutElement, scrollWidth - clientWidth);
+        break;
+    }
+
+    console.log(this.activeSlideIndex);
+  }
+
+  private setScrollBarPosition(element: HTMLElement, position: number) {
+    this.renderer.setProperty(element, 'scrollLeft', position);
+  }
 }
