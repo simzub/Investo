47cc55014a7a7e0f94129ab20dbc46d2a3f40dfc
simzub <78310955+simzub@users.noreply.github.com>
2023-10-12 16:19:37 +0300

    add animation duration input

--- src/app/reviews/reviews-column/reviews-column.component.ts
+++ src/app/reviews/reviews-column/reviews-column.component.ts
@@ -1,10 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-reviews-column',
   templateUrl: './reviews-column.component.html',
   styleUrls: ['./reviews-column.component.scss'],
 })
-export class ReviewsColumnComponent {
+export class ReviewsColumnComponent implements OnInit {
   @Input() reviews: any[] = [];
+  @Input() animationDuration: string = '25s';
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  ngOnInit() {
+    const columnContentElement =
+      this.el.nativeElement.querySelector('.column-content');
+
+    const columnContentOverlayElement = this.el.nativeElement.querySelector(
+      '.column-content-overlay'
+    );
+    // Check if the element is found before applying the animation
+    if (columnContentElement && columnContentOverlayElement) {
+      this.renderer.setStyle(
+        columnContentElement,
+        'animation-duration',
+        this.animationDuration
+      );
+      this.renderer.setStyle(
+        columnContentOverlayElement,
+        'animation-duration',
+        this.animationDuration
+      );
+    }
+  }
 }
