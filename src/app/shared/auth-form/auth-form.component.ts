75a76b6cddd4b60253b13d325b490b96cd9a108b
simzub <78310955+simzub@users.noreply.github.com>
2023-10-26 13:04:01 +0300

    moved part of the logic to the auth-form component and added the register component

--- src/app/shared/auth-form/auth-form.component.ts
+++ src/app/shared/auth-form/auth-form.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
-  styleUrls: ['./auth-form.component.scss']
+  styleUrls: ['./auth-form.component.scss'],
 })
 export class AuthFormComponent {
-
+  @Input() header: string = '';
+  @Input() descriptionBefore: string = '';
+  @Input() descriptionAfter: string = '';
+  @Input() routeLink: string = '';
+  @Input() link: string = '';
+  @Input() submitText: string = '';
 }
